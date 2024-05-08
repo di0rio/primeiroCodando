9ef2a1adf93dee6bbc8668c9aeef4e8aa140d2bb
di0rio <caua.diorio29@gmail.com>
2024-05-08 18:49:18 -0300

    operadores aritméticos, contendo adição, subtração, divisão, multiplicação...etc.

---
+++ operadores_aritméticos.py
@@ -0,0 +1,37 @@
+# operações matemáticas
+print('\nOperações básicas da matemáticas')
+print(1+1)
+print(1-1)
+print(1*1)
+
+# divisao e divisao inteira
+print (10 / 10) #float
+print (10 // 10) #int
+
+# módulo e exponenciação
+print(10 % 3) #resto 1
+
+print(2 ** 3)
+
+#python segue as normas matematicas
+print('\nSeguindo as normas matemáticas!')
+print(10 - 5 * 2)
+
+print((10 - 5) * 2)
+
+print(10 ** 2 * 2)
+
+print(10 ** (2 * 2))
+
+print(10 / 2 * 4)
+
+
+
+#PRÁTICA#
+print('\nPRATICANDO!')
+leite, achocolatado = 2.50, 7.60
+print(leite + achocolatado)
+print(leite * achocolatado)
+# print(leite / achocolatado)
+print(leite * 4 + achocolatado)
+print(leite % achocolatado)
