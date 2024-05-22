85e81c161ad92b55e642881a1f033eb0387b7cc9
di0rio <caua.diorio29@gmail.com>
2024-05-22 14:43:27 -0300

    ajustando comentários do código

--- funcao.py
+++ funcao.py
@@ -65,18 +65,9 @@ exibir_poema(
 def criar_carro(modelo, ano, placa, /, marca, motor, combustivel):
     print(modelo, ano, placa, marca, motor, combustivel)
 criar_carro("Palio", 1999, "ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")
-# criar_carro(modelo="Palio", ano=1999, placa="ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")  # inválido
-
-
-def criar_carro(modelo, ano, placa, /, *, marca, motor, combustivel):
-    print(modelo, ano, placa, marca, motor, combustivel)
-# criar_carro(modelo="Palio", ano=1999, placa="ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")  # inválido
-
 
 def somar(a, b):
     return a + b
-
-
 def exibir_resultado(a, b, funcao):
     resultado = funcao(a, b)
     print(f"O resultado da operação {a} + {b} = {resultado}")
@@ -88,7 +79,7 @@ def salario_bonus(bonus):
     global salario
     salario += bonus
     return salario
-salario_bonus(500)  # 2500
+salario_bonus(500)  
 
 
 def funcao(*args, **kw):
